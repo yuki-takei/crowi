92da5408d5f16849946d31bc73a037f70dc11611
Yuki Takei <yuki@weseek.co.jp>
2021-07-07 12:23:21 +0000

    add a controller for test

---
+++ packages/slackbot-proxy/src/controllers/http-errors-test.ts
@@ -0,0 +1,46 @@
+import {
+  Controller, Get, Inject, UseBefore,
+} from '@tsed/common';
+
+import createError from 'http-errors';
+import { CustomHttpError } from '~/models/errors';
+import { InstallerService } from '~/services/InstallerService';
+
+
+const middleware3 = () => {
+  throw new Error('This page is NOT processed by CustomHttpErrorFilter');
+};
+
+const middleware4 = () => {
+  throw new CustomHttpError(createError(404, 'Resource 4 does not exist!'));
+};
+
+@Controller('/http-errors-test')
+export class TopCtrl {
+
+  @Inject()
+  installerService: InstallerService;
+
+  @Get('/1')
+  async testCustomHttpError1(): Promise<any> {
+    throw new Error('This page is NOT processed by CustomHttpErrorFilter');
+  }
+
+  @Get('/2')
+  async testCustomHttpError2(): Promise<any> {
+    throw new CustomHttpError(createError(404, 'Resource 2 does not exist!'));
+  }
+
+  @Get('/3')
+  @UseBefore(middleware3)
+  async testCustomHttpError3(): Promise<any> {
+    return true;
+  }
+
+  @Get('/4')
+  @UseBefore(middleware4)
+  async testCustomHttpError4(): Promise<any> {
+    return true;
+  }
+
+}
